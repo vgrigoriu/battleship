d3ad491c055854910f436d79c7ce1030428ab30d
Victor Grigoriu <vgigoriu@gmail.com>
2011-11-14 22:09:04 +0200

    Tools for code analysis etc.

---
+++ tools/SetupHooks.ps1
@@ -0,0 +1,35 @@
+﻿$git = Get-Command git -CommandType Application -ErrorAction SilentlyContinue
+
+if (-not $git) {
+    Write-Host "Please install:";
+    Write-Host "`tgit from http://git-scm.com/download";
+    Write-Host "`tgit-tfs from https://github.com/spraints/git-tfs";
+    Write-Host "Optional:";
+    Write-Host "`tposh-git from https://github.com/dahlbyk/posh-git";
+    exit;
+}
+
+$isInside = git rev-parse --is-inside-work-tree 2>$null;
+if (-not $isInside) {
+    Write-Host "Please run inside a git working tree";
+    exit;
+}
+
+$gitDir = git rev-parse --git-dir;
+$topDir = git rev-parse --show-toplevel;
+$gitHooksDir = "$gitDir\hooks";
+$toolsDir = "$topDir\4.dev\tools";
+
+# remove .git\hooks if it exists
+if (Test-Path $gitHooksDir) {
+    if ((& $toolsDir\sysinternals\junction.exe $gitHooksDir) -match "No .* found") {
+        Remove-Item $gitHooksDir -Recurse;
+    } else {
+        & $toolsDir\sysinternals\junction.exe -d $gitHooksDir;
+    }
+}
+
+# link .git\hooks to tools\git-hooks in working tree
+& $toolsDir\sysinternals\junction.exe $gitHooksDir "$toolsDir\git-hooks"
+
+Write-Host "Done!"
---
+++ tools/git-hooks/pre-commit.ps1
@@ -0,0 +1,51 @@
+﻿## make sure everything is staged
+#$nonCommitedFiles = git diff --name-only
+#if ($nonCommitedFiles -ne $null) {
+#    Write-Host "The following files are changed but not staged:"
+#    Write-Host $nonCommitedFiles
+#    Write-Host "Commit will be cancelled"
+#    exit 3
+#}
+
+# build all the solutions under src
+$msbuild = Get-Command MSBuild.exe -CommandType Application -ErrorAction SilentlyContinue
+
+if (-not $msbuild) {
+    Write-Host "Please run in a Visual Studio environment"
+    Write-Host "(see http://bradwilson.typepad.com/blog/2010/03/vsvars2010ps1.html"
+    Write-Host "or http://stackoverflow.com/questions/2124753/)"
+    Write-Host "Commit will be cancelled"
+    exit 1
+}
+
+$workingDirectory = git rev-parse --show-toplevel
+if (-not $?) {
+    Write-Host "Not inside a git repository, quitting"
+    exit 4
+}
+
+# build all solutions
+Get-ChildItem $workingDirectory\4.Dev\src\ -Recurse -Include *.sln | ForEach-Object {
+    $solution = $_
+    Write-Host "Building $solution"
+    MSBuild.exe $solution /p:StyleCopTreatErrorsAsWarnings=false
+    if (-not $?) {
+        Write-Host "$solution did not build correctly"
+        Write-Host "Commit will be cancelled"
+        exit 2
+    }
+}
+
+# analyze all assemblies, run unit tests
+Get-ChildItem $workingDirectory\4.Dev\src\ -Recurse -Include *.csproj | ForEach-Object {
+    $project = $_
+    Write-Host "Verifying $project"
+    MsBuild.exe $project /target:VerifyCommit
+    if (-not $?) {
+        Write-Host "$project did not pass commit verification"
+        Write-Host "Commit will be cancelled"
+        exit 3
+    }
+}
+
+# exit with non-zero status code to stop the commit
